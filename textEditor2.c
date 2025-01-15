c9875e584bc7c981a380adccc5ed936184fa24ec
Arnav <arnav.saraf777@gmail.com>
2025-01-15 12:53:22 -0800

    Add files via upload

---
+++ textEditor2.c
@@ -0,0 +1,206 @@
+#include <windows.h>
+#include <Scintilla.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define IDC_MAIN_EDIT 100
+
+// Scintilla function pointers
+typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int msg, uptr_t wParam, sptr_t lParam);
+typedef sptr_t (*SciFnDirectStatus)(sptr_t ptr, unsigned int msg, uptr_t wParam, sptr_t lParam, int *status);
+
+// Global variables
+HMODULE hScintilla = NULL;
+SciFnDirect Scintilla_DirectFunction;
+
+// Function to create the Scintilla editor
+HWND CreateScintillaEditor(HWND hwndParent, HINSTANCE hInstance) {
+    // Load Scintilla DLL
+    hScintilla = LoadLibrary("Scintilla.dll");
+    if (!hScintilla) {
+        MessageBox(NULL, "Failed to load Scintilla.dll!", "Error", MB_ICONERROR | MB_OK);
+        return NULL;
+    }
+
+    // Get Scintilla's Direct Function
+    Scintilla_DirectFunction = (SciFnDirect)GetProcAddress(hScintilla, "Scintilla_DirectFunction");
+    if (!Scintilla_DirectFunction) {
+        MessageBox(NULL, "Failed to get Scintilla_DirectFunction!", "Error", MB_ICONERROR | MB_OK);
+        FreeLibrary(hScintilla);
+        return NULL;
+    }
+
+    // Register Scintilla class
+    WNDCLASS wc;
+    if (!GetClassInfo(hScintilla, "Scintilla", &wc)) {
+        MessageBox(NULL, "Scintilla class not registered!", "Error", MB_ICONERROR | MB_OK);
+        FreeLibrary(hScintilla);
+        return NULL;
+    }
+
+    // Create Scintilla window
+    HWND hwndScintilla = CreateWindowEx(
+        0,
+        "Scintilla",
+        "",
+        WS_CHILD | WS_VISIBLE | WS_TABSTOP | WS_CLIPCHILDREN,
+        0, 0, 0, 0,
+        hwndParent,
+        (HMENU)IDC_MAIN_EDIT,
+        hInstance,
+        NULL);
+
+    if (!hwndScintilla) {
+        DWORD error = GetLastError();
+        char errorMsg[256];
+        sprintf(errorMsg, "Failed to create Scintilla window! Error code: %lu", error);
+        MessageBox(NULL, errorMsg, "Error", MB_ICONERROR | MB_OK);
+        FreeLibrary(hScintilla);
+        return NULL;
+    }
+
+    return hwndScintilla;
+}
+
+// Function to send a message to Scintilla
+sptr_t SendEditor(HWND hwndScintilla, UINT Msg, WPARAM wParam, LPARAM lParam) {
+    return SendMessage(hwndScintilla, Msg, wParam, lParam);
+}
+
+// Window procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    static HWND hwndScintilla = NULL;
+    
+    switch (msg) {
+        case WM_CREATE: {
+            // Create Scintilla editor window
+            hwndScintilla = CreateScintillaEditor(hwnd, ((LPCREATESTRUCT)lParam)->hInstance);
+            if (!hwndScintilla) {
+                return -1;  // Fail window creation
+            }
+
+            // Initialize editor settings
+            SendEditor(hwndScintilla, SCI_STYLECLEARALL, 0, 0);
+            SendEditor(hwndScintilla, SCI_SETCODEPAGE, SC_CP_UTF8, 0);
+            
+            // Set up basic styling
+            SendEditor(hwndScintilla, SCI_STYLESETFORE, STYLE_DEFAULT, RGB(0, 0, 0));
+            SendEditor(hwndScintilla, SCI_STYLESETBACK, STYLE_DEFAULT, RGB(255, 255, 255));
+            
+            // Enable line numbers
+            SendEditor(hwndScintilla, SCI_SETMARGINTYPEN, 0, SC_MARGIN_NUMBER);
+            SendEditor(hwndScintilla, SCI_SETMARGINWIDTHN, 0, 45);
+
+            // Set basic features
+            SendEditor(hwndScintilla, SCI_SETUNDOCOLLECTION, 1, 0);
+            SendEditor(hwndScintilla, SCI_SETCARETLINEVISIBLE, 1, 0);
+            SendEditor(hwndScintilla, SCI_SETCARETLINEBACK, RGB(240, 240, 240), 0);
+            
+            // Set tab width
+            SendEditor(hwndScintilla, SCI_SETTABWIDTH, 4, 0);
+            
+            // Enable automatic indentation
+            SendEditor(hwndScintilla, SCI_SETINDENTATIONGUIDES, SC_IV_REAL, 0);
+            SendEditor(hwndScintilla, SCI_SETINDENT, 4, 0);
+            
+            // Set word wrap
+            SendEditor(hwndScintilla, SCI_SETWRAPMODE, SC_WRAP_WORD, 0);
+            
+            // Set font
+            SendEditor(hwndScintilla, SCI_STYLESETFONT, STYLE_DEFAULT, (LPARAM)"Consolas");
+            SendEditor(hwndScintilla, SCI_STYLESETSIZE, STYLE_DEFAULT, 10);
+            
+            return 0;
+        }
+        
+        case WM_SIZE: {
+            if (hwndScintilla) {
+                SetWindowPos(hwndScintilla,
+                    NULL,
+                    0, 0,
+                    LOWORD(lParam), HIWORD(lParam),
+                    SWP_NOZORDER);
+            }
+            return 0;
+        }
+
+        case WM_NOTIFY: {
+            LPNMHDR notification = (LPNMHDR)lParam;
+            if (notification->idFrom == IDC_MAIN_EDIT) {
+                SCNotification *scn = (SCNotification*)lParam;
+                switch (notification->code) {
+                    case SCN_CHARADDED:
+                    case SCN_MODIFIED:
+                        // Handle notifications if needed
+                        break;
+                }
+            }
+            return 0;
+        }
+
+        case WM_DESTROY:
+            if (hScintilla) {
+                FreeLibrary(hScintilla);
+            }
+            PostQuitMessage(0);
+            return 0;
+    }
+    
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow) {
+    
+    // Register window class
+    WNDCLASSEX wc = {
+        sizeof(WNDCLASSEX),
+        CS_HREDRAW | CS_VREDRAW,
+        WndProc,
+        0, 0,
+        hInstance,
+        LoadIcon(NULL, IDI_APPLICATION),
+        LoadCursor(NULL, IDC_ARROW),
+        (HBRUSH)(COLOR_WINDOW + 1),
+        NULL,
+        "ScintillaEditor",
+        LoadIcon(NULL, IDI_APPLICATION)
+    };
+    
+    if (!RegisterClassEx(&wc)) {
+        MessageBox(NULL, "Window Registration Failed!", "Error",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+    
+    // Create main window
+    HWND hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        "ScintillaEditor",
+        "Simple Scintilla Editor",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600,
+        NULL, NULL,
+        hInstance,
+        NULL
+    );
+    
+    if (hwnd == NULL) {
+        MessageBox(NULL, "Window Creation Failed!", "Error",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+    
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+    
+    // Message loop
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    
+    return msg.wParam;
+}
---
+++ texteditor.c
@@ -0,0 +1,217 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+// Helper Functions
+void swap(char *a, char *b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    if (TTF_Init() == -1) {
+        printf("TTF could not initialize! TTF_Error: %s\n", TTF_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 Text Input",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600, SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        TTF_Quit();
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        TTF_Quit();
+        SDL_Quit();
+        return 1;
+    }
+
+    TTF_Font* font = TTF_OpenFont("fonts/OpenSans-Regular.ttf", 24);
+    if (!font) {
+        printf("Failed to load font! TTF_Error: %s\n", TTF_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        TTF_Quit();
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Color textColor = {0, 0, 0, 255}; // Black color
+    char textBuffer[256] = ""; // Buffer to store user input
+    char tempBuffer[256]; // Temporary buffer for strtok operations
+    int quit = 0;
+	int cursor = 0;
+	int bufferIndex = 0;
+	int tmpBufferIndex = 0;
+    SDL_StartTextInput();
+
+    while (!quit) {
+        SDL_Event e;
+        
+        // Handle events
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            } else if (e.type == SDL_KEYDOWN) {
+                if (e.key.keysym.sym == SDLK_BACKSPACE && cursor > 0) {
+					textBuffer[cursor-1] = textBuffer[cursor];
+					cursor--;
+					for(int i=1; i<bufferIndex; i++){
+						textBuffer[cursor+i] = textBuffer[cursor+i+1];
+                    }					
+					textBuffer[bufferIndex] = '\0';
+                    bufferIndex--;
+                } else if (e.key.keysym.sym == SDLK_RETURN) {
+					tmpBufferIndex = bufferIndex; // Remember to reuse this code 
+					while(cursor < tmpBufferIndex){
+						swap(&textBuffer[tmpBufferIndex+1], &textBuffer[tmpBufferIndex]);
+						tmpBufferIndex--;
+                    }
+                    textBuffer[cursor] = '\n'; // Insert a newline character on Return key
+                    bufferIndex++;
+                    cursor++;
+                } else if (e.key.keysym.sym == SDLK_ESCAPE) {
+                    quit = 1; // Quit on Escape
+                } else if (e.key.keysym.sym == SDLK_LEFT && cursor > 0) {
+                    swap(&textBuffer[cursor - 1], &textBuffer[cursor]);
+                    --cursor;
+                } else if (e.key.keysym.sym == SDLK_RIGHT && cursor < bufferIndex) {
+                    swap(&textBuffer[cursor + 1], &textBuffer[cursor]);
+                    ++cursor;
+                } else if (e.key.keysym.sym == SDLK_UP) {
+
+					int i = 0;
+					int j;
+					int flag_1 = 0;
+					while (true){
+						if (textBuffer[cursor-1] == '\n' && flag_1 == 0){
+							j = i; 
+							flag_1 = 1;
+						}
+						else if((textBuffer[cursor-1] == '\n' && flag_1 == 1 ) || (cursor == 0 && flag_1 == 1)){
+							i = 0;
+							while (i<j){
+								i++;
+								swap(&textBuffer[cursor + 1], &textBuffer[cursor]);
+								cursor++;
+								if(textBuffer[cursor + 1] == '\n')
+									break;
+							}
+							flag_1 = 0;
+							break;
+						}
+						else if(cursor == 0)
+							break;
+						
+						i++;
+						swap(&textBuffer[cursor - 1], &textBuffer[cursor]);
+						cursor--;
+
+					}	
+				} else if (e.key.keysym.sym == SDLK_DOWN) {
+					if (cursor < bufferIndex - 1) {  // Ensure we're not at the end
+						int currentCol = 0;
+						int tempCursor = cursor;
+						
+						// Step 1: Count characters to the left until newline to get current column
+						while (tempCursor > 0 && textBuffer[tempCursor - 1] != '\n') {
+							tempCursor--;
+							currentCol++;
+						}
+						
+						// Step 2: Move cursor to the end of current line while swapping
+						while (cursor < bufferIndex && textBuffer[cursor + 1] != '\n') {
+							swap(&textBuffer[cursor], &textBuffer[cursor + 1]);
+							cursor++;
+						}
+						
+						if (cursor < bufferIndex - 1) {
+							// Step 3: Move past the newline
+							swap(&textBuffer[cursor], &textBuffer[cursor + 1]);
+							cursor++;
+							
+							// Step 4: Move right in the next line while swapping
+							int movedCol = 0;
+							while (movedCol < currentCol && 
+								   cursor < bufferIndex - 1 && 
+								   textBuffer[cursor + 1] != '\n') {
+								swap(&textBuffer[cursor], &textBuffer[cursor + 1]);
+								cursor++;
+								movedCol++;
+							}
+						}
+					}
+				}
+            } else if (e.type == SDL_TEXTINPUT) {
+                if (bufferIndex < sizeof(textBuffer) - 1) {
+					tmpBufferIndex = bufferIndex; // Remember to reuse this code
+					while(cursor < tmpBufferIndex){
+						swap(&textBuffer[tmpBufferIndex+1], &textBuffer[tmpBufferIndex]);
+						tmpBufferIndex--;
+                    }
+					textBuffer[cursor] = *e.text.text;					
+					bufferIndex++;
+                    cursor++;
+                }
+            }
+        }
+        textBuffer[cursor] = '|'; // Render cursor as '|'
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White background
+        SDL_RenderClear(renderer);
+
+        // Copy the buffer because strtok modifies the original string
+        strcpy(tempBuffer, textBuffer);
+        
+        // Tokenize and render text based on newline characters
+        char* textBufferToken = strtok(tempBuffer, "\n");
+        int y_off = 0;
+        int tokenCnt = 0;
+
+        while (textBufferToken != NULL) {
+            tokenCnt++;
+            SDL_Surface* textSurface = TTF_RenderText_Blended(font, textBufferToken, textColor);
+            if (textSurface) {
+                SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+                if (textTexture) {
+                    SDL_Rect textRect = {0, y_off, textSurface->w, textSurface->h};
+                    y_off += textSurface->h;
+                    
+                    SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+                    SDL_DestroyTexture(textTexture);
+                }
+                SDL_FreeSurface(textSurface);
+            }
+            textBufferToken = strtok(NULL, "\n");
+			printf("%d, %d \n",textSurface->w,textSurface->h);
+        }
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_StopTextInput();
+    TTF_CloseFont(font);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_Quit();
+    SDL_Quit();
+    return 0;
+}
+
